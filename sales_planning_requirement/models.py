0d371b73f3995ac25916d9f7c7e222c7db27aa61
Nareshkumar124 <nkk124421@gmail.com>
2026-06-04 19:06:51 +0530

    Use weekly sales planning procedure

--- sales_planning_requirement/models.py
+++ sales_planning_requirement/models.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 from django.utils import timezone
 
 
-PROCEDURE_NAME = "SALES PLANNING VS REQUIREMENT_OLD"
+PROCEDURE_NAME = "SALES PLANNING VS REQUIREMENT_WEEKLY"
 
 
 class SalesPlanningRequirementPermission(models.Model):
